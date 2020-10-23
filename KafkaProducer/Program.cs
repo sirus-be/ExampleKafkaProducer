772e4b61abc80bbf320b56a040007b9573a3e244
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2020-10-23 16:40:47 +0200

    first commit

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+using System.Threading.Tasks;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092", ClientId = "testjan" };
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                await producer.ProduceAsync("testjantopic", new Message<Null, string> { Value = "test jan" });
+            }
+        }
+    }
+}
